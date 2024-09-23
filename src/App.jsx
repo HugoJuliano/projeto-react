4a99cc4d6da3fd58fc03ac19a9ee30f1b826a55b
HugoJuliano <hugojuliano@hotmail.com.br>
2024-09-23 00:22:59 -0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./styles.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+export default function App() {
+  return (
+    
+      <div className="App">
+        <Header />
+
+        <Main />
+
+      </div>
+    
+  );
+}
+
+
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+export default function Main() {
+    return (
+      <main>
+        <h1>Curiosidades sobre o React</h1>
+        <ul className="main--facts">
+          <li>Foi lançado pela primeira vez em 2013</li>
+          <li>Foi originalmente criado por Jordan Walke</li>
+          <li>Tem mais de 100 mil estrelas no GitHub</li>
+          <li>É mantido pelo Facebook</li>
+          <li>
+            Capacita milhares de aplicativos corporativos, incluindo aplicativos
+            móveis
+          </li>
+        </ul>
+      </main>
+    );
+  }
